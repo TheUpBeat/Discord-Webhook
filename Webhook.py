cd7c6be0ddcf79f7b2d801f2975fad4967165f6f
TheUpBeat <TheUpBeat@protonmail.com>
2020-07-13 23:05:06 +0530

    Functional program for webhook. Fix all issues.

--- Webhook.py
+++ Webhook.py
@@ -8,115 +8,121 @@ choice = str(input("Enter \n N/n for Normal Message \n E/e for Embeded Message \
 if (choice == "N" or choice == "n"):
     data = {}
     content = (input("Enter the Message:\n"))
-    username = (input("Enter the Bot's Username (Press Enter to use the default settings): "))
-    avatar_url = (input("Enter the Bot's Avatar (url) (Press Enter to use the default settings): "))
-    tts = (input("Enter a TTS Message (Press Enter to use the default settings): "))
-    allowed_mentions = (input("Enter the Members to be mentioned (Press Enter to use the default settings): "))
-
-    if not username.isalpha():
-        pass
-    else:
-        data["username"] = username
-
-    if not avatar_url.isalpha():
-        pass
-    else:
-        data["avatar_url"] = avatar_url
-
-    if not tts.isalpha():
-        pass
-    else:
-        data["tts"] = tts
-
-    if not allowed_mentions.isalpha():
-        pass
-    else:
-        data["allowed_mentions"] = allowed_mentions
-
     data["content"] = content
 
+    username = (input("Enter the Bot's Username (Press Enter to use the default settings): "))
+    if not username == "":
+        data["username"] = username
+
+    avatar_url = (input("Enter the Bot's Avatar (url) (Press Enter to use the default settings): "))
+    if not avatar_url == "":
+        data["avatar_url"] = avatar_url
 
 elif (choice == "E" or choice == "e"):
     data = {}
     data["embeds"] = []
     embed = {}
-    embed["footer"] = []
-    foot = {}
 
     advancedornot = str(input("Enter \n B/b for Basic options for Embed \n A/a for More options for Embed \n"))
     if (advancedornot == 'B' or advancedornot == 'b'):
         title = input("Enter the title: ")
         description = input("Enter the description: ")
-        color = input("Enter the color code (Press Enter to use the default settings): ")
-        if not color.isalpha():
-            pass
-        else:
-            embed["color"] = color
+        color = (input("Enter the color code (Press Enter to use the default settings): "))
 
         embed["title"] = title
         embed["description"] = description
+        if not color == "":
+            embed["color"] = color
+
 
     elif (advancedornot == 'A' or advancedornot == 'a'):
-        title = input("Enter the title: ")
-        description = input("Enter the description: ")
-        color = input("Enter the color code (Press Enter to use the default settings): ")
-        footer = input("Enter C to add info to the footer (Press Enter to use the default settings): ")
 
-        if not footer.isalpha():
-            pass
-        else:
-            text = input("Enter the Footer information: ")
-            icon_url = input("Enter URL of the footer icon (Press Enter to use the default settings): ")
-            proxy_icon_url = input("Enter proxy URL of the footer icon (Press Enter to use the default settings): ")
-            if not icon_url.isalpha():
-                pass
-            else:
-                foot["icon_url"] = icon_url
+        username = (input("Enter the Bot's Username (Press Enter to use the default settings): "))
+        if not username == "":
+            data["username"] = username
 
-            if not proxy_icon_url.isalpha():
-                pass
-            else:
-                foot["proxy_icon_url"] = proxy_icon_url
+        avatar_url = (input("Enter the Bot's Avatar (url) (Press Enter to use the default settings): "))
+        if not avatar_url == "":
+            data["avatar_url"] = avatar_url
 
-            foot["text"] = text
+        author_name = input("Enter C of the Author (Press Enter to use the default settings): ")
+        if not author_name == "":
+            name = input("Author's Name: ")
+            URL = input("Author's URL: ")
+            iconurl = input("Icon URL: ")
 
-        image = input("Enter the URL of Image (Press Enter to use the default settings): ")
-        thumbnail = input("Enter the URL of Thumbnail (Press Enter to use the default settings): ")
-        video = input("Enter the URL of Video (Press Enter to use the default settings): ")
-        author = input("Enter the Name of the Author (Press Enter to use the default settings): ")
-        fields = input("Enter info in fields (Press Enter to use the default settings): ")
+            author = {}
 
-        if not color.isalpha():
-            pass
-        else:
-            embed["color"] = color
+            if not name == "":
+                author["name"] = name
 
+            if not URL == "":
+                author["url"] = URL
 
-        if not image.isalpha():
-            pass
-        else:
-            embed["image"] = image
-        if not thumbnail.isalpha():
-            pass
-        else:
-            embed["thumbnail"] = thumbnail
-        if not video.isalpha():
-            pass
-        else:
-            embed["video"] = video
-        if not author.isalpha():
-            pass
-        else:
+            if not iconurl == "":
+                author["icon_url"] = iconurl
+
             embed["author"] = author
-        if not fields.isalpha():
-            pass
-        else:
-            embed["fields"] = fields
 
+        title = input("Enter the title: ")
         embed["title"] = title
+
+        description = input("Enter the description: ")
         embed["description"] = description
 
-    embed["footer"].append(foot)
+        color = input("Enter the color code (Press Enter to use the default settings): ")
+        if not color == "":
+            embed["color"] = color
+
+        field = input("Enter number of fields (Press Enter to use the default settings): ")
+
+        if not field == "":
+            number = int(field)
+            embed ["fields"] = []
+            fields = {}
+
+            for i in range(number):
+                name = input("Name of the field: ")
+                fields["name"] = name
+                value = input("Value of the field: ")
+                fields["value"] = value
+                trueornot = input("Inline: \n T/t for True \n F/f for False \n")
+                if (trueornot == 'T' or trueornot == 't'):
+                    fields["inline"] = True
+                elif (trueornot == 'F' or trueornot == 'f'):
+                    fields["inline"] = False
+
+                embed["fields"].append(fields)
+
+        footer = input("Enter C to add info to the footer (Press Enter to use the default settings): ")
+
+        if not footer == "":
+            footer = {}
+            text = input("Footer Text: ")
+            icon_url = input("Footer Icon URL: ")
+
+            if not text == "":
+                footer["text"] = text
+
+            if not icon_url == "":
+                footer["icon_url"] = icon_url
+
+            embed["footer"] = footer
+
+        image_url = input("Enter the URL of Image (Press Enter to use the default settings): ")
+        if not image_url == "":
+            image = {}
+
+            image["url"] = image_url
+            embed["image"] = image
+
+        thumbnail_url = input("Enter the URL of Thumbnail (Press Enter to use the default settings): ")
+        if not thumbnail_url == "":
+            thumbnail = {}
+
+            thumbnail["url"] = thumbnail_url
+            embed["thumbnail"] = thumbnail
+
     data["embeds"].append(embed)
 
 
